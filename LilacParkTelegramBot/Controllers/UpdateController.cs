9dc1a8675dd1aa898dd96101a1b41b6ce0b98db9
Ruslan Musakalimov <ruslan.musakalimov@yva.ai>
2020-11-28 11:27:05 +0300

    chore: rename projects

--- LilacParkTelegramBot/Controllers/UpdateController.cs
+++ LilacParkTelegramBot/Controllers/UpdateController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using LylacParkServices;
-using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using LylacParkServices.Interfaces;
 using TelegramBot;
 
-namespace LilacParkTelegrammBot.Controllers
+namespace LilacParkTelegramBot.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
--- LilacParkTelegramBot/Startup.cs
+++ LilacParkTelegramBot/Startup.cs
@@ -13,7 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace LilacParkTelegrammBot
+namespace LilacParkTelegramBot
 {
     public class Startup
     {
